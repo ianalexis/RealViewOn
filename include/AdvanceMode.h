37614b6bd6ec1c3e7a724f8c9c9033eddc6cb59a
Ian Bassi <ian.bassi@outlook.com>
2025-02-24 16:50:15 -0300

    Reapply "Preparando para registros complejos"
    This reverts commit 0602fc941de8dd1e547e4471108e92503ba7fc63.

--- include/AdvanceMode.h
+++ include/AdvanceMode.h
@@ -15,19 +15,38 @@ private:
     const string rutaBase = "[HKEY_CURRENT_USER\\SOFTWARE\\SolidWorks\\SOLIDWORKS ";
     string rutaVersionada = "";
     string completeBase (string complement);
+
+    struct multiRegSetting {
+        string path;
+        std::vector<string> value;
+    };
     
     struct RegSetting {
         string name;
-        string content;
+        multiRegSetting content;
     };
 
     const std::vector<RegSetting> regSettings = {
-        {"Reverse Mouse Wheel", "General]\n\"Reverse Zoom Direction\"=dword:00000001"},
-        {"DarkMode", "General]\n\"Color Theme ID\"=dword:00000003\n;Light: 1, Medium: 2, Dark: 3, Medium Light: 4."},
-        {"FPS Viewer", "Performance]\n\"OpenGL Print Statistics\"=dword:00000001"},
-        {"Advance Menu Options (Recommended)", "TBD]\n\"TBD\"=dword:TBD"}, // TODO: Definir
-        {"Performance Enhance graphics (Recommended)", "\\Performance]\n\"Use Performance Pipeline 2020\"=dword:00000001\n\"Use GPU Silhouette Edges\"=dword:00000001"},
-        {"Full AntiAliasing (Recommended)", "Edges]\n\"Show Anti Alias Shaded Edges\"=dword:00000000\n\"Show Fullscene Anti Alias\"=dword:00000001"}
+        {"Reverse Mouse Wheel",
+            {"General",
+                {"Reverse Zoom Direction\"=dword:00000001"}}},
+        {"DarkMode",
+            {"General",
+                {"Color Theme ID\"=dword:00000003\n;Light: 1, Medium: 2, Dark: 3, Medium Light: 4."}}},
+        {"FPS Viewer",
+            {"Performance",
+                {"OpenGL Print Statistics\"=dword:00000001"}}},
+        {"Performance Enhance graphics (Recommended)",
+            {"Performance",
+                {"Use Performance Pipeline 2020\"=dword:00000001",
+                "Use GPU Silhouette Edges\"=dword:00000001"}}},
+        {"Full AntiAliasing (Recommended)",
+            {"Edges",
+                {"Show Anti Alias Shaded Edges\"=dword:00000000",
+                "Show Fullscene Anti Alias\"=dword:00000001"}}},
+        {"Advance Menu Options (Recommended)",
+            {"TBD",
+                {"TBD\"=dword:TBD"}}}
     };
-};;
+};
 
