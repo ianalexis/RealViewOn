ecc4db3a877d24d9bb88dcf8ab19319f36445ef8
Ian Bassi <ian.bassi@outlook.com>
2025-03-05 15:40:02 -0300

    v2.0.7.1

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
 #define FILE_VERSION_MAJOR 2
 #define FILE_VERSION_MINOR 0
 #define FILE_VERSION_PATCH 7
-#define FILE_VERSION_BUILD 0
+#define FILE_VERSION_BUILD 1
 #define FILE_VERSION_STABLE 1 // 1 for stable, 0 for prerelease
 
 #define STRINGIFY(x) #x
